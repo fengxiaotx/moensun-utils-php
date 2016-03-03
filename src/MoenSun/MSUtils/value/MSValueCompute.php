dd0c790f60e0cf9b5caaff315605981a32a0470b
Bane.Shi <fengxiaotx@163.com>
2016-03-04 00:05:25 +0800

    [Bane.Shi]

--- src/MoenSun/MSUtils/value/MSValueCompute.php
+++ src/MoenSun/MSUtils/value/MSValueCompute.php
@@ -12,8 +12,63 @@ namespace MoenSun\MSUtils\value;
 
 class MSValueCompute
 {
+    /**
+     * 分页查询的时候获取起始条数
+     * @param $page
+     * @param $pageSize
+     * @return int
+     */
     public static function getStartRow($page,$pageSize){
         $page=($page<1)?1:$page;
         return ($page-1)*$pageSize;
     }
+
+    /**
+     * 分页查询时， 获取总页数
+     * @param $count
+     * @param $pageSize
+     * @return int
+     */
+    public static function getPageCount($count,$pageSize){
+        if( $count <= $pageSize){
+            return 1;
+        }else{
+            return intval($count/$pageSize)+(($count%$pageSize==0)?0:1);
+        }
+    }
+
+    /**
+     * 列表页的分页菜单的起始页
+     * @param $currentPage
+     * @param $pageSize
+     * @return int
+     */
+    public static function getStartPage($currentPage,$pageSize){
+        if($currentPage <= $pageSize){
+            return 1;
+        }else {
+            return  $currentPage -($currentPage%$pageSize-1) ;
+        }
+    }
+
+    /**
+     * 列表页分页菜单的结束页
+     * @param $count
+     * @param $currentPage
+     * @param $pageSize
+     * @return int
+     */
+    public static function getEndPage($count,$currentPage,$pageSize){
+        $pageCount = self::getPageCount($count,$pageSize);
+        if($currentPage <= $pageSize ){
+            return $pageSize;
+        }elseif( $end = $currentPage+( $pageSize- ($currentPage%$pageSize)) ){
+            if($end >= $pageCount){
+                return $pageCount;
+            }else {
+                return $end;
+            }
+        }
+    }
+
 }
