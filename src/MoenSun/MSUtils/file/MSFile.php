0a8114598fe1b4ae80eb731aac058a228ab2fe33
Bane.Shi <fengxiaotx@163.com>
2016-05-05 18:07:52 +0800

    [Bane.Shi] cesi config

--- src/MoenSun/MSUtils/file/MSFile.php
+++ src/MoenSun/MSUtils/file/MSFile.php
@@ -10,6 +10,8 @@
 namespace MoenSun\MSUtils\file;
 
 
+use MoenSun\MSUtils\random\MSRandom;
+
 class MSFile
 {
     /**
@@ -25,5 +27,23 @@ class MSFile
             }
         }
         return $returnStr;
+    }
+
+    public static function datePath($fileName,$prefix = ''){
+        $d = explode('-',date('Y-y-m-d-H-i-s'));
+        $fileName = preg_replace("/[\|\?\"\<\>\/\*\\\\]+/", '', $fileName);
+        return str_replace('//','/',$prefix.$d[0].'/'.$d[2].'/').$fileName;
+    }
+
+    public static function getExtensionByType($type){
+        $extension = strtolower(substr($type,strrpos($type,'/')+1));
+        if($extension == 'jpeg'){
+            $extension = 'jpg';
+        }
+        return $extension;
+    }
+
+    public static function randomFileName($type){
+        return MSRandom::getUUID().'.'.self::getExtensionByType($type);
     }
 }
