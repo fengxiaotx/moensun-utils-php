e90fe2d6d74301f9dc28738a1c121e93f8abf06b
Bane.Shi <fengxiaotx@163.com>
2016-05-15 22:53:38 +0800

    [Bane.Shi]

--- src/MoenSun/MSUtils/value/MSValueCheck.php
+++ src/MoenSun/MSUtils/value/MSValueCheck.php
@@ -12,10 +12,33 @@ namespace MoenSun\MSUtils\value;
 
 class MSValueCheck
 {
+    /**
+     * 判断是不是sql查询条件
+     * @param $value
+     * @return bool
+     */
     public static function isSqlCondition($value){
         if($value ||  $value == "0" || is_int($value)){
             return true;
         }
         return false;
+    }
+
+    /**
+     * 判断数组中是否有符合sql查询的条件
+     * @param array $conditions
+     * @return bool
+     */
+    public static function hasCondition(array $conditions){
+        $isTrue = false;
+        if(!empty($conditions) && count($conditions)>0){
+            foreach ($conditions as $k=>$v){
+                if(self::isSqlCondition($v)){
+                    $isTrue = true;
+                    break;
+                }
+            }
+        }
+        return $isTrue;
     }
 }
